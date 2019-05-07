6b22121516ab4d2d5eb6f3202eec80b76d0cde0e
Dawid Dziurla <dawidd0811@gmail.com>
2019-05-07 17:11:55 +0200

    signal: add comments

--- signal.go
+++ signal.go
@@ -4,11 +4,18 @@ package appindicator
 import "C"
 
 const (
-	SignalNewIcon           = C.APP_INDICATOR_SIGNAL_NEW_ICON
-	SignalNewAttentionIcon  = C.APP_INDICATOR_SIGNAL_NEW_ATTENTION_ICON
-	SignalNewStatus         = C.APP_INDICATOR_SIGNAL_NEW_STATUS
-	SignalNewLabel          = C.APP_INDICATOR_SIGNAL_NEW_LABEL
+	// SignalNewIcon defines that indicator has new icon.
+	SignalNewIcon = C.APP_INDICATOR_SIGNAL_NEW_ICON
+	// SignalNewAttentionIcon defines that indicator has new attention icon.
+	SignalNewAttentionIcon = C.APP_INDICATOR_SIGNAL_NEW_ATTENTION_ICON
+	// SignalNewStatus defines that indicator has new status.
+	SignalNewStatus = C.APP_INDICATOR_SIGNAL_NEW_STATUS
+	// SignalNewLabel defines that indicator has new label.
+	SignalNewLabel = C.APP_INDICATOR_SIGNAL_NEW_LABEL
+	// SignalConnectionChanged defines that indicator's connection has changed.
 	SignalConnectionChanged = C.APP_INDICATOR_SIGNAL_CONNECTION_CHANGED
-	SignalNewIconThemePath  = C.APP_INDICATOR_SIGNAL_NEW_ICON_THEME_PATH
-	SignalScrollEvent       = C.APP_INDICATOR_SIGNAL_SCROLL_EVENT
+	// SignalNewIconThemePath defines that indicator has new icon theme path.
+	SignalNewIconThemePath = C.APP_INDICATOR_SIGNAL_NEW_ICON_THEME_PATH
+	// SignalScrollEvent defines that user scrolled on indicator.
+	SignalScrollEvent = C.APP_INDICATOR_SIGNAL_SCROLL_EVENT
 )
