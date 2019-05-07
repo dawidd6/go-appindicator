ace2825a4f35ee8040dfbbc7e4f74cb8979a52eb
Dawid Dziurla <dawidd0811@gmail.com>
2019-05-07 17:05:11 +0200

    signal: remove custom type
    so it's easier to use in gtk.Connect()

--- signal.go
+++ signal.go
@@ -4,13 +4,11 @@ package appindicator
 import "C"
 
 const (
-	SignalNewIcon           Signal = C.APP_INDICATOR_SIGNAL_NEW_ICON
-	SignalNewAttentionIcon  Signal = C.APP_INDICATOR_SIGNAL_NEW_ATTENTION_ICON
-	SignalNewStatus         Signal = C.APP_INDICATOR_SIGNAL_NEW_STATUS
-	SignalNewLabel          Signal = C.APP_INDICATOR_SIGNAL_NEW_LABEL
-	SignalConnectionChanged Signal = C.APP_INDICATOR_SIGNAL_CONNECTION_CHANGED
-	SignalNewIconThemePath  Signal = C.APP_INDICATOR_SIGNAL_NEW_ICON_THEME_PATH
-	SignalScrollEvent       Signal = C.APP_INDICATOR_SIGNAL_SCROLL_EVENT
+	SignalNewIcon           = C.APP_INDICATOR_SIGNAL_NEW_ICON
+	SignalNewAttentionIcon  = C.APP_INDICATOR_SIGNAL_NEW_ATTENTION_ICON
+	SignalNewStatus         = C.APP_INDICATOR_SIGNAL_NEW_STATUS
+	SignalNewLabel          = C.APP_INDICATOR_SIGNAL_NEW_LABEL
+	SignalConnectionChanged = C.APP_INDICATOR_SIGNAL_CONNECTION_CHANGED
+	SignalNewIconThemePath  = C.APP_INDICATOR_SIGNAL_NEW_ICON_THEME_PATH
+	SignalScrollEvent       = C.APP_INDICATOR_SIGNAL_SCROLL_EVENT
 )
-
-type Signal string
