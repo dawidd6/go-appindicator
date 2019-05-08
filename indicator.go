909c7539122df42013c3a83102d2fb6bc952d42f
Dawid Dziurla <dawidd0811@gmail.com>
2019-05-08 14:58:47 +0200

    indicator: fix memory leaks
    closes: #6

--- indicator.go
+++ indicator.go
@@ -1,6 +1,7 @@
 package appindicator
 
 // #cgo pkg-config: appindicator3-0.1
+// #include <stdlib.h>
 // #include <libappindicator/app-indicator.h>
 import "C"
 import (
@@ -31,23 +32,37 @@ func (indicator *Indicator) Object() *glib.Object {
 
 // New creates a fresh indicator.
 func New(id, iconName string, category Category) *Indicator {
+	idC := C.CString(id)
+	defer C.free(unsafe.Pointer(idC))
+	iconNameC := C.CString(iconName)
+	defer C.free(unsafe.Pointer(iconNameC))
+	categoryC := (C.AppIndicatorCategory)(category)
+
 	return &Indicator{
 		indicator: C.app_indicator_new(
-			C.CString(id),
-			C.CString(iconName),
-			category.native(),
+			idC,
+			iconNameC,
+			categoryC,
 		),
 	}
 }
 
 // NewWithPath creates a fresh indicator with custom icon theme path.
 func NewWithPath(id, iconName string, category Category, iconThemePath string) *Indicator {
+	idC := C.CString(id)
+	defer C.free(unsafe.Pointer(idC))
+	iconNameC := C.CString(iconName)
+	defer C.free(unsafe.Pointer(iconNameC))
+	categoryC := (C.AppIndicatorCategory)(category)
+	iconThemePathC := C.CString(iconThemePath)
+	defer C.free(unsafe.Pointer(iconThemePathC))
+
 	return &Indicator{
 		indicator: C.app_indicator_new_with_path(
-			C.CString(id),
-			C.CString(iconName),
-			category.native(),
-			C.CString(iconThemePath),
+			idC,
+			iconNameC,
+			categoryC,
+			iconThemePathC,
 		),
 	}
 }
@@ -56,26 +71,36 @@ func NewWithPath(id, iconName string, category Category, iconThemePath string) *
 
 // SetStatus sets status of indicator.
 func (indicator *Indicator) SetStatus(status Status) {
+	statusC := (C.AppIndicatorStatus)(status)
+
 	C.app_indicator_set_status(
 		indicator.indicator,
-		status.native(),
+		statusC,
 	)
 }
 
 // SetAttentionIcon sets attention icon of indicator.
 func (indicator *Indicator) SetAttentionIcon(iconName string) {
+	iconNameC := C.CString(iconName)
+	defer C.free(unsafe.Pointer(iconNameC))
+
 	C.app_indicator_set_attention_icon(
 		indicator.indicator,
-		C.CString(iconName),
+		iconNameC,
 	)
 }
 
 // SetAttentionIconFull sets attention icon of indicator with description.
 func (indicator *Indicator) SetAttentionIconFull(iconName, iconDesc string) {
+	iconNameC := C.CString(iconName)
+	defer C.free(unsafe.Pointer(iconNameC))
+	iconDescC := C.CString(iconDesc)
+	defer C.free(unsafe.Pointer(iconDescC))
+
 	C.app_indicator_set_attention_icon_full(
 		indicator.indicator,
-		C.CString(iconName),
-		C.CString(iconDesc),
+		iconNameC,
+		iconDescC,
 	)
 }
 
@@ -91,18 +116,26 @@ func (indicator *Indicator) SetMenu(menu *gtk.Menu) {
 
 // SetIcon sets icon of indicator.
 func (indicator *Indicator) SetIcon(iconName string) {
+	iconNameC := C.CString(iconName)
+	defer C.free(unsafe.Pointer(iconNameC))
+
 	C.app_indicator_set_icon(
 		indicator.indicator,
-		C.CString(iconName),
+		iconNameC,
 	)
 }
 
 // SetIconFull sets icon of indicator with description.
 func (indicator *Indicator) SetIconFull(iconName, iconDesc string) {
+	iconNameC := C.CString(iconName)
+	defer C.free(unsafe.Pointer(iconNameC))
+	iconDescC := C.CString(iconDesc)
+	defer C.free(unsafe.Pointer(iconDescC))
+
 	C.app_indicator_set_icon_full(
 		indicator.indicator,
-		C.CString(iconName),
-		C.CString(iconDesc),
+		iconNameC,
+		iconDescC,
 	)
 }
 
@@ -111,18 +144,26 @@ func (indicator *Indicator) SetIconFull(iconName, iconDesc string) {
 // Second parameter "guide" is used to set maximum width for label.
 // Don't know if it works. Feel free to pass empty string.
 func (indicator *Indicator) SetLabel(label, guide string) {
+	labelC := C.CString(label)
+	defer C.free(unsafe.Pointer(labelC))
+	guideC := C.CString(guide)
+	defer C.free(unsafe.Pointer(guideC))
+
 	C.app_indicator_set_label(
 		indicator.indicator,
-		C.CString(label),
-		C.CString(guide),
+		labelC,
+		guideC,
 	)
 }
 
 // SetIconThemePath sets icon theme path of indicator.
 func (indicator *Indicator) SetIconThemePath(iconThemePath string) {
+	iconThemePathC := C.CString(iconThemePath)
+	defer C.free(unsafe.Pointer(iconThemePathC))
+
 	C.app_indicator_set_icon_theme_path(
 		indicator.indicator,
-		C.CString(iconThemePath),
+		iconThemePathC,
 	)
 }
 
@@ -130,9 +171,11 @@ func (indicator *Indicator) SetIconThemePath(iconThemePath string) {
 //
 // It may or may not work.
 func (indicator *Indicator) SetOrderingIndex(orderingIndex uint) {
+	orderingIndexC := C.guint(orderingIndex)
+
 	C.app_indicator_set_ordering_index(
 		indicator.indicator,
-		C.guint(orderingIndex),
+		orderingIndexC,
 	)
 }
 
@@ -147,9 +190,12 @@ func (indicator *Indicator) SetSecondaryActivateTarget(menuItem *gtk.MenuItem) {
 
 // SetTitle sets title of indicator.
 func (indicator *Indicator) SetTitle(title string) {
+	titleC := C.CString(title)
+	defer C.free(unsafe.Pointer(titleC))
+
 	C.app_indicator_set_title(
 		indicator.indicator,
-		C.CString(title),
+		titleC,
 	)
 }
 
@@ -294,9 +340,14 @@ func (indicator *Indicator) GetSecondaryActivateTarget() *gtk.MenuItem {
 //
 // To be honest I don't know how it works.
 func (indicator *Indicator) BuildMenuFromDesktop(desktopFile, desktopProfile string) {
+	desktopFileC := C.CString(desktopFile)
+	defer C.free(unsafe.Pointer(desktopFileC))
+	desktopProfileC := C.CString(desktopProfile)
+	defer C.free(unsafe.Pointer(desktopProfileC))
+
 	C.app_indicator_build_menu_from_desktop(
 		indicator.indicator,
-		C.CString(desktopFile),
-		C.CString(desktopProfile),
+		desktopFileC,
+		desktopProfileC,
 	)
 }
